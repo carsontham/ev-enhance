a9008c6eee104ac852d0d1cb19607d1b2a52451a
jichong <151018920+jichong-tay@users.noreply.github.com>
2024-04-06 17:17:35 +0800

    setup db connection

--- app/cmd/main.go
+++ app/cmd/main.go
@@ -1,8 +1,22 @@
 package main
 
-import "ev-enhance/app/cmd/api"
+import (
+	"ev-enhance/app/cmd/api"
+	"ev-enhance/db"
+	"log"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	err := godotenv.Load("app.env")
+	if err != nil {
+		log.Fatal("Some error occured. Err: ", err)
+	}
+
+	db.InitDB()
+	defer db.CloseDB()
+
 	s := api.NewAPIServer(":3000")
 	s.SetupRoutes()
 	s.StartServer()
